efe8e7e8c60e18a903ca4d9779ad104703b14db8
Ovid- OdedBE <ovid.odedbe@gmail.com>
2026-04-05 00:39:13 +0300

    remade segment

---
+++ include/bsp/data_types.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <utils.h>
+
+namespace bsp {
+    struct Segment {
+        glm::vec2 start;
+        glm::vec2 end;
+    };
+}
---
+++ src/bsp/data_types.cpp
@@ -0,0 +1,1 @@
+#include <bsp/data_types.h>
