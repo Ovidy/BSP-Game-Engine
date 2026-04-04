31584c3dfba3a52bf9b59e9cc832f364f5ba6fed
Ovid- OdedBE <ovid.odedbe@gmail.com>
2026-04-05 00:46:04 +0300

    Add level data

--- include/bsp/level_data.h
+++ include/bsp/level_data.h
@@ -4,6 +4,16 @@
 
 namespace bsp {
     struct LevelData {
+        LevelData(const std::vector<Segment>& segments);
+        LevelData(const std::vector<std::pair<glm::vec2, glm::vec2>>& segments);
+
+        LevelData() = default;
+        LevelData(const LevelData&) = default;
+        LevelData(LevelData&&) = default;
+        LevelData& operator=(LevelData&&) = default;
+        LevelData& operator=(const LevelData&) = default;
+        ~LevelData() = default;
+
         std::vector<Segment> segments;
     };
 }
