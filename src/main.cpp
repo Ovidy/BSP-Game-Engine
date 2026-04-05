d934882f546d5c7ffcaae314744f18d71688f606
Ovid- OdedBE <ovid.odedbe@gmail.com>
2026-04-05 22:09:24 +0300

    Delta time

--- src/main.cpp
+++ src/main.cpp
@@ -48,6 +48,7 @@ int main ()
 	// game loop
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
+		deltaTime = GetFrameTime();
 		renderer.update();
 		renderer.render();
 	}
