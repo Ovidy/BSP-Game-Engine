4fb0730f5f757ec55335ab9ee30213de674a3565
Ovid- OdedBE <ovid.odedbe@gmail.com>
2026-04-05 21:59:03 +0300

    laod

--- src/bsp/renderer.cpp
+++ src/bsp/renderer.cpp
@@ -48,7 +48,8 @@ namespace bsp {
 
     void Renderer::load_level(const std::vector<std::pair<glm::vec2, glm::vec2>>& segments) {
         level_data = LevelData(segments);
-        map_renderer.load_level_data(level_data, tree_builder);
         tree_builder.build(segments);
+        tree_traverser.set_root(tree_builder.get_root());
+        map_renderer.load_level_data(level_data, tree_builder);
     }
 }
