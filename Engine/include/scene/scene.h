376b362070bbdf4f944820af0b60680418c728cd
Ovid- OdedBE <ovid.odedbe@gmail.com>
2026-05-23 21:59:26 +0300

    Add sprite and texture management

--- Engine/include/scene/scene.h
+++ Engine/include/scene/scene.h
@@ -2,6 +2,8 @@
 
 #include <entt/entt.hpp>
 
+#include <bsp/data_types.h>
+
 class Scene {
 public:
     // Constructors and destructors
@@ -13,7 +15,12 @@ public:
     void destroy_entity(const entt::entity& entity);
 
     // Texture Management
-    
+    void load_texture(const entt::entity& entity, const std::string& file_path);
+    const Texture2D& get_texture(const entt::entity& entity) const;
+
+    // Sprite Management
+    void load_sprite(const entt::entity& entity, const bsp::Sprite& sprite);
+    const bsp::Sprite& get_sprite(const entt::entity& entity) const;
 
 private:
     entt::registry registry;
--- Engine/src/scene/scene.cpp
+++ Engine/src/scene/scene.cpp
@@ -1,5 +1,7 @@
 #include <scene/scene.h>
 
+#include <raylib.h>
+
 Scene::Scene() {
 
 }
@@ -14,4 +16,20 @@ entt::entity Scene::create_entity() {
 
 void Scene::destroy_entity(const entt::entity& entity) {
     registry.destroy(entity);
+}
+
+void Scene::load_texture(const entt::entity& entity, const std::string& file_path) {
+    registry.emplace_or_replace<Texture2D>(entity, LoadTexture(file_path.c_str()));
+}
+
+const Texture2D& Scene::get_texture(const entt::entity& entity) const {
+    return registry.get<Texture2D>(entity);
+}
+
+void Scene::load_sprite(const entt::entity& entity, const bsp::Sprite& sprite) {
+    registry.emplace_or_replace<bsp::Sprite>(entity, sprite);
+}
+
+const bsp::Sprite& Scene::get_sprite(const entt::entity& entity) const {
+    return registry.get<bsp::Sprite>(entity);
 }
